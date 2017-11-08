1fb02638bb804703fe1e92bfea515978f5b2cd0b
Javier Santos <javier_santos_chaparro@hotmail.com>
2017-11-09 00:12:12 +0100

    Docker image for tensorflow with python mtcnn evaluation scripts

---
+++ Docker/Tensorflow/PythonScripts/RabbitMqServer.py
@@ -0,0 +1,70 @@
+import argparse
+import asyncio
+import aioamqp
+import json
+import time
+
+@asyncio.coroutine
+def on_request(channel, body, envelope, properties):
+    # n = int(body)
+
+    print(" body: " + body.decode('utf-8'))
+    #response = fib(n)
+
+    yield from channel.basic_publish(
+        payload=json.dumps({'IsSuccess': True, 'errorMessage': '', 'OutputValues': [{'name':'conv4-1', 'value': [[[[0,1,2,3]]]] }] }),
+        exchange_name='',
+        routing_key=properties.reply_to,
+        properties={
+            'correlation_id': properties.correlation_id,
+        },
+    )
+
+    yield from channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
+
+@asyncio.coroutine
+def on_request2(channel, body, envelope, properties):
+    # n = int(body)
+
+    print(" body 2: " + body.decode('utf-8'))
+    #response = fib(n)
+
+    yield from channel.basic_publish(
+        payload=json.dumps({'IsSuccess': True, 'errorMessage': '', 'OutputValues': [{'name':'conv4-1', 'value': [[[[0,1,2,3]]]] }] }),
+        exchange_name='',
+        routing_key=properties.reply_to,
+        properties={
+            'correlation_id': properties.correlation_id,
+        },
+    )
+
+    yield from channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
+    
+
+@asyncio.coroutine
+def rpc_server(hostname):
+
+    transport, protocol = yield from aioamqp.connect(host=hostname, port=5672)
+
+    print('Connected to RabbitMQ, loading exchange and channels')
+
+    channel = yield from protocol.channel()
+
+    # yield from channel.queue_declare(queue_name='evaluate.onet')
+    yield from channel.basic_qos(prefetch_count=1, prefetch_size=0, connection_global=False)
+    yield from channel.basic_consume(on_request, queue_name='evaluate.onet')
+    yield from channel.basic_consume(on_request2, queue_name='evaluate.pnet')
+    print(" [x] Awaiting RPC requests")
+
+print('waiting to RabbitMQ to be online')
+time.sleep(10)
+
+parser = argparse.ArgumentParser(description='Run RabbitMQ server to process the different messages from the service bus.')
+parser.add_argument("servicebushost", help="Machine name of the service bus ")
+args = parser.parse_args()
+
+print('Connecting to RabbitMQ on ' + args.servicebushost)
+
+event_loop = asyncio.get_event_loop()
+event_loop.run_until_complete(rpc_server(args.servicebushost))
+event_loop.run_forever()
