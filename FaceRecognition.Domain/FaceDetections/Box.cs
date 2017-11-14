49179be80e280a991b1395de0c4170fc4f864f84
Javier Santos <javier_santos_chaparro@hotmail.com>
2017-11-14 19:27:12 +0100

    Bounding box algorithm for Mtcnn

---
+++ FaceRecognition.Domain/FaceDetections/Box.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaceRecognition.Common.NoLoh.SpanImplementation;
+
+namespace FaceRecognition.Domain.FaceDetections
+{
+    public class Box
+    {
+        public int MinX { get; protected set; }
+        public int MinY { get; protected set; }
+        public int MaxX { get; protected set; }
+        public int MaxY { get; protected set; }
+        public float Score { get; protected set; }
+
+        public static IEnumerable<Box> CreateFromMtcnnHeatMap(NoLohArray3D<float> probabilities, NoLohArray4D<float> heatmap, float scale, float threshold)
+        {
+            const float stride = 2f;
+            const float cellSize = 12f;
+            const int zIndex = 1;
+            var positions = new ConcurrentBag<(int X, int Y, float Score)>();
+
+            Parallel.For(0, probabilities.XLength, x =>
+            {
+                for (int y = 0; y < probabilities.YLength; y++)
+                {
+                    if (probabilities[x, y, zIndex] > threshold)
+                    {
+                        positions.Add((x, y, probabilities[x, y, zIndex]));
+                    }
+                }
+            });
+
+            if (positions.Count == 0)
+            {
+                return Enumerable.Empty<Box>();
+            }
+
+            var boundingBoxes = new List<Box>();
+            foreach (var position in positions)
+            {
+                boundingBoxes.Add(new Box
+                {
+                    MinX = (int)Math.Round((stride * position.X) / scale),
+                    MinY = (int)Math.Round((stride * position.Y) / scale),
+                    MaxX = (int)Math.Round((stride * position.X + cellSize) / scale),
+                    MaxY = (int)Math.Round((stride * position.Y + cellSize) / scale),
+                    Score = position.Score
+                });
+            }
+            return boundingBoxes;
+        }
+    }
+}
---
+++ FaceRecognition.UnitTests/Domain/FaceDetections/BoxShould.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FaceRecognition.Common;
+using FaceRecognition.Common.NoLoh.SpanImplementation;
+using FaceRecognition.Domain.FaceDetections;
+using FluentAssertions;
+using Xunit;
+
+namespace FaceRecognition.UnitTests.Domain.FaceDetections
+{
+    public class BoxShould
+    {
+        [Fact]
+        public void ReturnBoundingBoxes_WhenAHeatmapProbabilityIsSupplied()
+        {
+            const float threshold = 0.7f;
+            const float scale = 0.5f;
+            using (var probabilities = new NoLohArray3D<float>(50, 50, 2))
+            using (var heatmap = new NoLohArray4D<float>(1, 50, 50, 4))
+            {
+                probabilities[2, 4, 1] = 0.8f;
+                probabilities[4, 8, 1] = 0.9f;
+
+                var boxes = Box.CreateFromMtcnnHeatMap(probabilities, heatmap, scale, threshold);
+
+                boxes.Should().NotBeEmpty();
+                boxes.Should().HaveCount(2);
+                boxes.Should().Contain(item =>
+                    item.MinX == 8 && item.MaxX == 32 && item.MinY == 16 && item.MaxY == 40 && Math.Abs(item.Score - 0.8f) < Bootstrapper.FloatTolerance);
+                boxes.Should().Contain(item =>
+                    item.MinX == 16 && item.MaxX == 40 && item.MinY == 32 && item.MaxY == 56 && Math.Abs(item.Score - 0.9f) < Bootstrapper.FloatTolerance);
+            }
+        }
+    }
+}
